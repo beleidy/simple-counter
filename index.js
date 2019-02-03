0ee2b784137b27fd40931e74aac0b099d6c0cc6c
Amr Elbeleidy <amr@elbeleidy.me>
2019-02-03 13:55:14 +0000

    latest version

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+var counter = 0;
+
+function message() {
+  return `Simple counter app -- this instace has been viewed ${counter++} times`;
+}
+
+app.get("/*", (req, res) => res.send(message()));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
